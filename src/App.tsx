9e9633f3b83f16bc94b8871ea77b4b8b6c04bee6
Darko Joska <darko.joska@bild-studio.net>
2021-12-17 09:23:53 +0100

    Init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello threre!</h2>
+      <h2 className='text-3xl font-bold underline'>Hello there!</h2>
     </div>
   );
 }
